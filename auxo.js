ea55cbf13ffd9ed4d669f4638738b8681f409b46
take3315 <take3315.dfgc@gmail.com>
2023-05-09 22:11:40 +0700

    added graphQL

--- auxo.js
+++ auxo.js
@@ -1,6 +1,12 @@
+
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import Web3 from 'web3';
+import pkg from '@apollo/client';
+const { ApolloClient, InMemoryCache, createHttpLink, gql } = pkg;
 dotenv.config();
+const infuraAPI = process.env.INFURA_API_KEY;
+const web3 = new Web3(`https://mainnet.infura.io/v3/${infuraAPI}`);
 const addresses = [
   '0x069c0Ed12dB7199c1DdAF73b94de75AAe8061d33',
   '0xc72fbD264b40D88E445bcf82663D63FF21e722AF'
@@ -12,16 +18,32 @@ const authheaderZapper = { "Authorization": "Basic " + Buffer.from(zapperAPI + "
 const paramsCovalent = { "method": "GET", "headers": authheaderCovalent };
 const paramsZapper = { "method": "GET", "headers": authheaderZapper };
 const batchSize = 10;
-
+const httpLink = createHttpLink({
+  uri: 'https://api.thegraph.com/subgraphs/name/jordaniza/auxo-staking',
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 async function getholderdemographics() {
-  const holderList = [];
   const holderInfo = [];
   const sumByAppId = {};
   const countByAppId = {};
-  const uniqueAddresses = {
+  const holderAddresses = {
     [addresses[0]]: new Set(),
-    [addresses[1]]: new Set()
+    [addresses[1]]: new Set(),
+    stakerPRV: new Set()
   };
+  const uniqueHolders = new Set();
+
+  const stakerData = await fetchPRVStakers();
+  const stakers = stakerData.prvstakingBalances;
+
+  for (const staker of stakers) {
+    const stakerAddress = staker.account.id;
+    holderAddresses.stakerPRV.add(stakerAddress);
+    uniqueHolders.add(stakerAddress);
+  }
 
   for (const address of addresses) {
     const urlCovalent = `https://api.covalenthq.com/v1/eth-mainnet/tokens/${address}/token_holders_v2/`;
@@ -30,11 +52,15 @@ async function getholderdemographics() {
     if (list.data) {
       for (let i = 0; i < list.data.items.length; i++) {
         const holder = list.data.items[i].address;
-        holderList.push(holder);
-        uniqueAddresses[address].add(holder); 
+        holderAddresses[address].add(holder);
+        if (!uniqueHolders.has(holder)) {
+          uniqueHolders.add(holder);
+        }
       }
     }
   }
+
+  const holderList = Array.from(uniqueHolders);
 
   for (let j = 0; j < holderList.length; j += batchSize) {
     const batchAddresses = holderList.slice(j, j + batchSize);
@@ -44,10 +70,11 @@ async function getholderdemographics() {
       holderInfo.push([
         userInfo[k].appId,
         parseFloat(userInfo[k].balanceUSD)
-      ])
+      ]);
     }
     await new Promise(resolve => setTimeout(resolve, 100));
   }
+
 
   holderInfo.forEach(info => {
     const appId = info[0];
@@ -64,53 +91,68 @@ async function getholderdemographics() {
     }
   });
 
-  const topByCount = Object.keys(countByAppId).map(appId => {
-    return [
-      appId,
-      countByAppId[appId],
-      sumByAppId[appId].toFixed(2)
-    ];
-  }).sort((a, b) => b[1] - a[1]).slice(0, 20);
+  const topByCount = Object.keys(countByAppId)
+    .map(appId => [appId, countByAppId[appId], sumByAppId[appId].toFixed(2)])
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 20);
 
-  const topBySumBalance = Object.keys(sumByAppId).map(appId => {
-    return [
-      appId,
-      countByAppId[appId],
-      sumByAppId[appId].toFixed(2)
-    ];
-  }).sort((a, b) => b[2] - a[2]).slice(0, 20);
+  const topBySumBalance = Object.keys(sumByAppId)
+    .map(appId => [appId, countByAppId[appId], sumByAppId[appId].toFixed(2)])
+    .sort((a, b) => b[2] - a[2])
+    .slice(0, 20);
 
-
-  console.log("Top 20 by usd term:", topBySumBalance);
+  console.log("Top 20 by USD term:", topBySumBalance);
   console.log("Top 20 by count:", topByCount);
-  console.log("Count of unique addresses for", addresses[0], ":", uniqueAddresses[addresses[0]].size);
-  console.log("Count of unique addresses for", addresses[1], ":", uniqueAddresses[addresses[1]].size);
+  console.log("Count of ARV holder", ":", holderAddresses[addresses[0]].size);
+  console.log("Count of PRV holder", ":", holderAddresses[addresses[1]].size);
+  console.log("Count of PRV staker :", holderAddresses.stakerPRV.size);
+  console.log("Count of unique addresses :", uniqueHolders.size);
 }
 
 getholderdemographics();
 
-/*
-import Web3 from 'web3';
-const infuraAPI = process.env.INFURA_API_KEY;
-const web3 = new Web3(`https://mainnet.infura.io/v3/${infuraAPI}`);
-async function getDepositors() {
+async function getCurrentBlockNumber() {
   try {
-    const proxyContractABI = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [{ "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "uint256", "name": "withdrawAmount", "type": "uint256" }], "name": "InvalidEmptyBalance", "type": "error" }, { "inputs": [{ "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "InvalidWithdrawalAmount", "type": "error" }, { "inputs": [], "name": "TransferFailed", "type": "error" }, { "inputs": [], "name": "ZeroAmount", "type": "error" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "depositor", "type": "address" }, { "indexed": true, "internalType": "address", "name": "receiver", "type": "address" }, { "indexed": true, "internalType": "uint8", "name": "epoch", "type": "uint8" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "Deposited", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "user", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "EmergencyWithdraw", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "depositor", "type": "address" }, { "indexed": true, "internalType": "uint8", "name": "epoch", "type": "uint8" }], "name": "Exited", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "uint8", "name": "version", "type": "uint8" }], "name": "Initialized", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "uint8", "name": "newEpochId", "type": "uint8" }, { "indexed": false, "internalType": "uint256", "name": "startedTimestamp", "type": "uint256" }], "name": "NewEpoch", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "account", "type": "address" }], "name": "Paused", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "bytes32", "name": "previousAdminRole", "type": "bytes32" }, { "indexed": true, "internalType": "bytes32", "name": "newAdminRole", "type": "bytes32" }], "name": "RoleAdminChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }], "name": "RoleGranted", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }], "name": "RoleRevoked", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "account", "type": "address" }], "name": "Unpaused", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "depositor", "type": "address" }, { "indexed": true, "internalType": "uint8", "name": "epoch", "type": "uint8" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "Withdrawn", "type": "event" }, { "inputs": [], "name": "DEFAULT_ADMIN_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "OPERATOR_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "activateNextEpoch", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "currentEpochId", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_amount", "type": "uint256" }], "name": "deposit", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_amount", "type": "uint256" }, { "internalType": "address", "name": "_receiver", "type": "address" }], "name": "depositFor", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_amount", "type": "uint256" }, { "internalType": "address", "name": "_receiver", "type": "address" }, { "internalType": "uint256", "name": "_deadline", "type": "uint256" }, { "internalType": "uint8", "name": "v", "type": "uint8" }, { "internalType": "bytes32", "name": "r", "type": "bytes32" }, { "internalType": "bytes32", "name": "s", "type": "bytes32" }], "name": "depositForWithSignature", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_amount", "type": "uint256" }, { "internalType": "uint256", "name": "_deadline", "type": "uint256" }, { "internalType": "uint8", "name": "v", "type": "uint8" }, { "internalType": "bytes32", "name": "r", "type": "bytes32" }, { "internalType": "bytes32", "name": "s", "type": "bytes32" }], "name": "depositWithSignature", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "emergencyWithdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "epochBalances", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "epochPendingBalance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "getActivations", "outputs": [{ "components": [{ "internalType": "uint256", "name": "_value", "type": "uint256" }], "internalType": "struct Bitfields.Bitfield", "name": "", "type": "tuple" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "getActiveBalanceForUser", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getCurrentEpochBalance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint8", "name": "_epochId", "type": "uint8" }], "name": "getEpochBalanceWithProjection", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getEpochBalances", "outputs": [{ "internalType": "uint256[]", "name": "", "type": "uint256[]" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "getPendingBalanceForUser", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getProjectedNextEpochBalance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }], "name": "getRoleAdmin", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "getTotalBalanceForUser", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "getUserStakingData", "outputs": [{ "components": [{ "components": [{ "internalType": "uint256", "name": "_value", "type": "uint256" }], "internalType": "struct Bitfields.Bitfield", "name": "activations", "type": "tuple" }, { "internalType": "uint8", "name": "epochWritten", "type": "uint8" }, { "internalType": "uint120", "name": "pending", "type": "uint120" }, { "internalType": "uint120", "name": "active", "type": "uint120" }], "internalType": "struct IRollStaker.UserStake", "name": "", "type": "tuple" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "grantRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "hasRole", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_stakingToken", "type": "address" }], "name": "initialize", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "lastEpochUserWasActive", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "pause", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "paused", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "quit", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "renounceRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "revokeRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "stakingToken", "outputs": [{ "internalType": "contract IERC20Upgradeable", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" }], "name": "supportsInterface", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "unpause", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "userIsActive", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }, { "internalType": "uint8", "name": "_epoch", "type": "uint8" }], "name": "userIsActiveForEpoch", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "userStakes", "outputs": [{ "components": [{ "internalType": "uint256", "name": "_value", "type": "uint256" }], "internalType": "struct Bitfields.Bitfield", "name": "activations", "type": "tuple" }, { "internalType": "uint8", "name": "epochWritten", "type": "uint8" }, { "internalType": "uint120", "name": "pending", "type": "uint120" }, { "internalType": "uint120", "name": "active", "type": "uint120" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_amount", "type": "uint256" }], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
-
-    const proxyContractAddress = '0x096b4F2253a430F33edc9B8e6A8e1d2fb4faA317';
-    const proxyContract = new web3.eth.Contract(proxyContractABI, proxyContractAddress);
-    const currentEpochId = await proxyContract.methods.currentEpochId().call();
-
-    if (currentEpochId > 0) {
-      const epochBalances = await proxyContract.methods.getEpochBalances().call();
-      const depositorAddresses = epochBalances.map(balance => balance.depositor);
-      console.log('Depositor Addresses:', depositorAddresses);
-    } else {
-      console.log('No epoch has started yet');
-    }
+    const blockNumber = await web3.eth.getBlockNumber();
+    return blockNumber;
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error getting current block number:', error);
+    throw error;
   }
 }
-getDepositors();
-*/
+async function fetchPRVStakers() {
+  const blockNumber = await getCurrentBlockNumber();
+
+  const query = gql`
+    query PRVStakers($block: Int, $skip: Int) {
+      prvstakingBalances(
+        skip: $skip,
+        block: { number: $block },
+        where: { value_not: "0" }
+      ) {
+        account {
+          id
+        }
+        value
+        valueExact
+      }
+    }
+  `;
+
+  const variables = {
+    block: blockNumber,
+    skip: 0,
+  };
+
+  try {
+    const { data } = await client.query({
+      query: query,
+      variables: variables,
+    });
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching PRV stakers:', error);
+    throw error;
+  }
+}
